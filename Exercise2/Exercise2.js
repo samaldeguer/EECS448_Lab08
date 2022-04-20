0a3ab419e7fd80dc6e1733631e96571261f12946
Sam Aldeguer <aldeguersam@gmail.com>
2022-04-20 00:00:33 -0500

    Create Exercise 2 - Slideshow

--- Exercise2/Exercise2.js
+++ Exercise2/Exercise2.js
@@ -1,1 +1,30 @@
+var currentImage = 1;
 
+function previous()
+{
+	document.getElementById(currentImage).style.display = 'none';
+	if (currentImage == 1)
+	{
+		currentImage = 5;
+	}
+	else
+	{
+		currentImage = currentImage - 1;
+	}
+	document.getElementById(currentImage).style.display = 'block';
+}
+
+function next()
+{
+	document.getElementById(currentImage).style.display = 'none';
+	if (currentImage == 5)
+	{
+		currentImage = 1;
+	}
+	else
+	{
+		currentImage = currentImage + 1;
+	}
+	document.getElementById(currentImage).style.display = 'block';
+
+}
