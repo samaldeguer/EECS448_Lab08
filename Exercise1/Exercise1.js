52f8a1e335451ab05214d48ca39cc62dd7ddc651
Sam Aldeguer <aldeguersam@gmail.com>
2022-04-19 23:50:57 -0500

    Create Exercise 1 - Password Validator

--- Exercise1/Exercise1.js
+++ Exercise1/Exercise1.js
@@ -1,1 +1,13 @@
-
+function validate()
+{
+	var passwordInput = document.getElementById("password");
+	var verifyInput = document.getElementById("verify");
+	if (passwordInput.value != verifyInput.value)
+	{
+		alert("The passwords entered do not match");
+	}
+	else if (passwordInput.value.length < 8)
+	{
+		alert("Password must be at least 8 characters long");
+	}
+}
