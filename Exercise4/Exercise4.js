f56c6f4a68c223593a33ccf92d0620e7e6942c88
Sam Aldeguer <aldeguersam@gmail.com>
2022-04-20 01:37:38 -0500

    Create Exercise 4 - CSS Manipulation

--- Exercise4/Exercise4.js
+++ Exercise4/Exercise4.js
@@ -1,1 +1,13 @@
-
+function makeChanges()
+{
+	var redBorder = document.getElementById("redBorder").value;
+	var greenBorder = document.getElementById("greenBorder").value;
+	var blueBorder = document.getElementById("blueBorder").value;
+	var redBackground = document.getElementById("redBackground").value;
+	var greenBackground = document.getElementById("greenBackground").value;
+	var blueBackground = document.getElementById("blueBackground").value;
+	var widthBorder = document.getElementById("widthBorder").value;
+	document.getElementById("text").style.borderColor = "rgb(" + redBorder + "," + greenBorder + "," + blueBorder + ")";
+	document.getElementById("text").style.borderWidth = widthBorder + "px";
+	document.getElementById("text").style.backgroundColor = "rgb(" + redBackground + "," + greenBackground + "," + blueBackground + ")";
+}
